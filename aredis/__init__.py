e9d3f597905882402396ce5ec43753e534892a3d
NoneGG <jason0916phoenix@gmail.com>
2016-12-25 15:23:54 +0800

    portation almost done, test is needed

--- aredis/__init__.py
+++ aredis/__init__.py
@@ -1,4 +1,31 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-__author__ = 'chenming@bilibili.com'
+from aredis.client import Redis, StrictRedis
+from aredis.connection import (
+    Connection,
+    UnixDomainSocketConnection
+)
+from aredis.pool import ConnectionPool
+from redis.exceptions import (
+    AuthenticationError,
+    BusyLoadingError,
+    ConnectionError,
+    DataError,
+    InvalidResponse,
+    PubSubError,
+    ReadOnlyError,
+    RedisError,
+    ResponseError,
+    TimeoutError,
+    WatchError
+)
 
+
+__version__ = '1.0.0'
+VERSION = tuple(map(int, __version__.split('.')))
+
+__all__ = [
+    'Redis', 'StrictRedis', 'ConnectionPool',
+    'Connection', 'UnixDomainSocketConnection',
+    'AuthenticationError', 'BusyLoadingError', 'ConnectionError', 'DataError',
+    'InvalidResponse', 'PubSubError', 'ReadOnlyError', 'RedisError',
+    'ResponseError', 'TimeoutError', 'WatchError'
+]
