db62b9c619eb858aa5a5f803e84d583d94140eab
NoneGG <jason0916phoenix@gmail.com>
2017-04-30 14:20:28 +0800

    add test on utils

--- tests/cluster/test_utils.py
+++ tests/cluster/test_utils.py
@@ -4,17 +4,17 @@
 from __future__ import with_statement
 
 # rediscluster imports
-from rediscluster.exceptions import (
+from aredis.commands.cluster import parse_cluster_slots
+from aredis.exceptions import (
     RedisClusterException, ClusterDownError
 )
-from rediscluster.utils import (
-    string_keys_to_dict,
+from aredis.utils import (
+    list_keys_to_dict,
     dict_merge,
     blocked_command,
     merge_result,
     first_key,
     clusterdown_wrapper,
-    parse_cluster_slots,
 )
 
 # 3rd party imports
@@ -55,12 +55,6 @@ def test_parse_cluster_slots():
           2) (integer) 10000
           3) "dc152a08b4cf1f2a0baf775fb86ad0938cb907dc"
     """
-    mock_response = [
-        [0, 5460, ['172.17.0.2', 7000], ['172.17.0.2', 7003]],
-        [5461, 10922, ['172.17.0.2', 7001], ['172.17.0.2', 7004]],
-        [10923, 16383, ['172.17.0.2', 7002], ['172.17.0.2', 7005]]
-    ]
-    parse_cluster_slots(mock_response)
 
     extended_mock_response = [
         [0, 5460, ['172.17.0.2', 7000, 'ffd36d8d7cb10d813f81f9662a835f6beea72677'], ['172.17.0.2', 7003, '5c15b69186017ddc25ebfac81e74694fc0c1a160']],
@@ -70,13 +64,6 @@ def test_parse_cluster_slots():
 
     parse_cluster_slots(extended_mock_response)
 
-    mock_binary_response = [
-        [0, 5460, [b('172.17.0.2'), 7000], [b('172.17.0.2'), 7003]],
-        [5461, 10922, [b('172.17.0.2'), 7001], [b('172.17.0.2'), 7004]],
-        [10923, 16383, [b('172.17.0.2'), 7002], [b('172.17.0.2'), 7005]]
-    ]
-    parse_cluster_slots(mock_binary_response)
-
     extended_mock_binary_response = [
         [0, 5460, [b('172.17.0.2'), 7000, b('ffd36d8d7cb10d813f81f9662a835f6beea72677')], [b('172.17.0.2'), 7003, b('5c15b69186017ddc25ebfac81e74694fc0c1a160')]],
         [5461, 10922, [b('172.17.0.2'), 7001, b('069cda388c7c41c62abe892d9e0a2d55fbf5ffd5')], [b('172.17.0.2'), 7004, b('dc152a08b4cf1f2a0baf775fb86ad0938cb907dc')]],
@@ -84,20 +71,42 @@ def test_parse_cluster_slots():
     ]
 
     extended_mock_parsed = {
-        (0, 5460): {'master': ('172.17.0.2', 7000), 'slaves': [('172.17.0.2', 7003)]},
-        (5461, 10922): {'master': ('172.17.0.2', 7001),
-                        'slaves': [('172.17.0.2', 7004)]},
-        (10923, 16383): {'master': ('172.17.0.2', 7002),
-                         'slaves': [('172.17.0.2', 7005)]}
+        (0, 5460): [
+            {'host': b'172.17.0.2',
+             'node_id': b'ffd36d8d7cb10d813f81f9662a835f6beea72677',
+             'port': 7000,
+             'server_type': 'master'},
+            {'host': b'172.17.0.2',
+             'node_id': b'5c15b69186017ddc25ebfac81e74694fc0c1a160',
+             'port': 7003,
+             'server_type': 'slave'}],
+        (5461, 10922): [
+            {'host': b'172.17.0.2',
+             'node_id': b'069cda388c7c41c62abe892d9e0a2d55fbf5ffd5',
+             'port': 7001,
+             'server_type': 'master'},
+            {'host': b'172.17.0.2',
+             'node_id': b'dc152a08b4cf1f2a0baf775fb86ad0938cb907dc',
+             'port': 7004,
+             'server_type': 'slave'}],
+        (10923, 16383): [
+            {'host': b'172.17.0.2',
+             'node_id': b'3588b4cf9fc72d57bb262a024747797ead0cf7ea',
+             'port': 7002,
+             'server_type': 'master'},
+            {'host': b'172.17.0.2',
+             'node_id': b'a72c02c7d85f4ec3145ab2c411eefc0812aa96b0',
+             'port': 7005,
+             'server_type': 'slave'}]
     }
 
     assert parse_cluster_slots(extended_mock_binary_response) == extended_mock_parsed
 
 
-def test_string_keys_to():
+def test_list_keys_to_dict():
     def mock_true():
         return True
-    assert string_keys_to_dict(["FOO", "BAR"], mock_true) == {"FOO": mock_true, "BAR": mock_true}
+    assert list_keys_to_dict(["FOO", "BAR"], mock_true) == {"FOO": mock_true, "BAR": mock_true}
 
 
 def test_dict_merge():
@@ -107,9 +116,8 @@ def test_dict_merge():
     assert dict_merge(a, b, c) == {"a": 1, "b": 2, "c": 3}
 
 
-def test_dict_merge_value_error():
-    with pytest.raises(ValueError):
-        dict_merge([])
+def test_dict_merge_empty_list():
+    assert dict_merge([]) == {}
 
 
 def test_blocked_command():
@@ -119,33 +127,33 @@ def test_blocked_command():
 
 
 def test_merge_result():
-    assert merge_result("foobar", {"a": [1, 2, 3], "b": [4, 5, 6]}) == [1, 2, 3, 4, 5, 6]
-    assert merge_result("foobar", {"a": [1, 2, 3], "b": [1, 2, 3]}) == [1, 2, 3]
+    assert merge_result({"a": [1, 2, 3], "b": [4, 5, 6]}) == [1, 2, 3, 4, 5, 6]
+    assert merge_result({"a": [1, 2, 3], "b": [1, 2, 3]}) == [1, 2, 3]
 
 
 def test_merge_result_value_error():
     with pytest.raises(ValueError):
-        merge_result("foobar", [])
+        merge_result([])
 
 
 def test_first_key():
-    assert first_key("foobar", {"foo": 1}) == 1
+    assert first_key({"foo": 1}) == 1
 
     with pytest.raises(RedisClusterException) as ex:
-        first_key("foobar", {"foo": 1, "bar": 2})
-    assert unicode(ex.value).startswith("More then 1 result from command: foobar")
+        first_key({"foo": 1, "bar": 2})
+    assert unicode(ex.value).startswith("More then 1 result from command")
 
 
 def test_first_key_value_error():
     with pytest.raises(ValueError):
-        first_key("foobar", None)
+        first_key(None)
 
-
-def test_clusterdown_wrapper():
+@pytest.mark.asyncio(forbid_global_loop=True)
+async def test_clusterdown_wrapper():
     @clusterdown_wrapper
     def bad_func():
         raise ClusterDownError("CLUSTERDOWN")
 
     with pytest.raises(ClusterDownError) as cex:
-        bad_func()
+        await bad_func()
     assert unicode(cex.value).startswith("CLUSTERDOWN error. Unable to rebuild the cluster")
