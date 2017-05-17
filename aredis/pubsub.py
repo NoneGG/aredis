53e2f18507996631557c675b9c0dfec96926a6ec
chenming <chenming@bilibili.com>
2017-05-17 18:02:24 +0800

    fix bug in pubsub

--- aredis/pubsub.py
+++ aredis/pubsub.py
@@ -71,7 +71,6 @@ class PubSub(object):
         # NOTE: don't parse the response in this function -- it could pull a
         # legitimate message off the stack if the connection is already
         # subscribed to one or more channels
-        await self.connection_pool.initialize()
 
         if self.connection is None:
             self.connection = self.connection_pool.get_connection()
