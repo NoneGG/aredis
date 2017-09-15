a39b2b532ca142bfc6a55af086481dd9dc615858
chenming <chenming@bilibili.com>
2017-09-15 11:17:22 +0800

    add example explaining how to use aredis with curio

---
+++ examples/use_with_curio.py
@@ -0,0 +1,20 @@
+import curio
+
+import aredis
+
+
+async def aio_child():
+    redis = aredis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+    await redis.flushdb()
+    await redis.set('bar', 'foo')
+    bar = await redis.get('bar')
+    return bar
+
+
+async def wrapper():
+    async with curio.bridge.AsyncioLoop() as loop:
+        return await loop.run_asyncio(aio_child)
+
+
+if __name__ == '__main__':
+    print(curio.run(wrapper))
