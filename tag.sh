b15de2c4772d6d02b7bde271ab987759f57d11a8
Ali-Akber Saifee <ali@indydevs.org>
2022-01-05 14:23:21 -0800

    Add release scripts

---
+++ tag.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+dryrun=0
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --dryrun )    dryrun=1
+                           ;;
+    esac
+    shift
+done
+
+rm -rf build dist
+last_tag=$(git tag | sort -nr | head -n 1)
+echo current version:$(python setup.py --version), current tag: $last_tag
+read -p "new version:" new_version
+last_portion=$(grep -P "^Changelog$" HISTORY.rst -5 | grep -P "^v\d+.\d+")
+changelog_file=/var/tmp/coredis.newchangelog
+new_changelog_heading="v${new_version}"
+new_changelog_heading_sep=$(python -c "print('-'*len('$new_changelog_heading'))")
+echo $new_changelog_heading > $changelog_file
+echo $new_changelog_heading_sep >> $changelog_file
+echo "Release Date: `date +"%Y-%m-%d"`" >> $changelog_file
+python -c "print(open('HISTORY.rst').read().replace('$last_portion', open('$changelog_file').read() +'\n' +  '$last_portion'))" > HISTORY.rst.new
+cp HISTORY.rst.new HISTORY.rst
+vim -O HISTORY.rst <(echo \# vim:filetype=git;git log $last_tag..HEAD --format='* %s (%h)%n%b' | sed -E '/^\*/! s/(.*)/    \1/g')
+if rst2html.py HISTORY.rst > /dev/null
+then
+    echo "tagging $new_version"
+    git add HISTORY.rst
+    git commit -m "updating changelog for  ${new_version}"
+    git tag -s ${new_version} -m "tagging version ${new_version}"
+    python setup.py build sdist
+    test $dryrun != 1 && twine upload dist/*
+    rm HISTORY.rst.new
+else
+    echo changelog has errors. skipping tag.
+fi;
+
